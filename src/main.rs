f70e74326446cc19cdf4a6da6bfa5b11b414608e
metal-face <hughesbryan3000@gmail.com>
2023-11-08 18:27:50 -0500

    gluestick and yew experimented with

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use gloo::console::log;
+use gloo::dialogs::alert;
+use gloo::timers::callback::Timeout;
+use wasm_bindgen::JsValue;
 use yew::prelude::*;
 use yew::{classes, html};
 
@@ -9,17 +13,29 @@ fn App() -> Html {
         move |_| {
             let value = *counter + 1;
             counter.set(value);
+            alert("balls in your face sonnnn")
         }
     };
 
     html! {
-        <div >
-            <button class={classes!("bg-red-100")} {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
+        <div class="flex flex-col h-screen bg-400-red">
+            <nav class="bg-green-400 h-16 px-8 py-2">
+                <div class="container flex mx-auto gap-6 items-center h-full">
+                    <button {onclick}>{ "+1" }</button>
+                    <p>{ *counter }</p>
+                </div>
+            </nav>
         </div>
     }
 }
 
 fn main() {
+    let timeout = Timeout::new(1_000, move || {
+        let object = JsValue::from("any JsValue can be logged");
+        log!("text", object);
+    });
+
+    // Since we don't plan on cancelling the timeout, call `forget`.
+    timeout.forget();
     yew::Renderer::<App>::new().render();
 }
