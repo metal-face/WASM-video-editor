197e8acbb8fe23c15577dad54f2b28b2c06085d9
metal-face <hughesbryan3000@gmail.com>
2023-11-13 16:41:50 -0500

    space removed

--- src/views/Home.rs
+++ src/views/Home.rs
@@ -21,7 +21,7 @@ pub fn home() -> Html {
 
     html! {
         <div class="bg-pink-400 p-2 h-full w-full flex flex-col justify-center">
-            <div class=" h-fit w-full flex justify-center align-middle">
+            <div class="h-fit w-full flex justify-center align-middle">
                 <button type="button" onclick={subtract} class="bg-slate-400 rounded p-1 m-1 h-fit shadow-xl">{"Minus One"}</button>
                 <p class="h-fit w-12 p-1 rounded shadow-xl bg-yellow-400 text-black text-center m-1" >{ *counter }</p>
                 <button type="button" class="h-fit w-fit p-1 bg-slate-400 rounded m-1 cursor-pointer shadow-lg" onclick={add}>{ "Add One" }</button>
