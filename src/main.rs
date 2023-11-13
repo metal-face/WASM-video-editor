b455d9ae3d7f084a983112a0afc408430aa981d6
metal-face <hughesbryan3000@gmail.com>
2023-11-12 23:53:13 -0500

    routing defined

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 mod router;
+mod views;
 
 use crate::router::index::{switch, Route};
 use yew::{function_component, html, Html};
 use yew_router::prelude::*;
 
-#[function_component(App)]
+#[function_component(Main)]
 pub fn app() -> Html {
     html! {
         <BrowserRouter>
-            <Switch<Route> render={Switch::render(switch)} />
+            <Switch<Route> render={switch} />
         </BrowserRouter>
     }
 }
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+    yew::Renderer::<Main>::new().render();
 }
