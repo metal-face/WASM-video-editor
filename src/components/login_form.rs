10cf17a55e688be7ee522bdc7ce3b0a1e2c90ab2
metal-face <hughesbryan3000@gmail.com>
2023-11-13 16:42:15 -0500

    login form defined

--- src/components/login_form.rs
+++ src/components/login_form.rs
@@ -3,18 +3,19 @@ use yew::{function_component, html, Html};
 #[function_component(LoginForm)]
 pub fn login_form() -> Html {
     html! {
-        <div class="h-full w-full flex justify-center align-middle">
-            <div class="w-1/2 h-1/2 flex outline">
-                <div>
-                    <label for="username">{"Username"}</label>
-                    <input id="username" type="text" placeholder="Username" />
-                </div>
-                <div>
-                    <label for="password">{"Password"}</label>
-                    <input id="password" type="password" placeholder="Password" />
+        <div class="h-full w-full flex justify-center items-center">
+            <div class="w-1/2 h-1/2 flex flex-col items-center outline">
+                <div class="h-full w-full flex flex-col justify-center items-center ">
+                    <div class="flex flex-col">
+                        <label class="text-center" for="username">{"Username"}</label>
+                        <input class="rounded p-1 shadow-" id="username" type="text" placeholder="Username" />
+                    </div>
+                    <div class="flex flex-col">
+                        <label class="text-center" for="password">{"Password"}</label>
+                        <input class="rounded p-1" id="password" type="password" placeholder="Password" />
+                    </div>
                 </div>
             </div>
         </div>
-
     }
 }
