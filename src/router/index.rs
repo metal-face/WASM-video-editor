5b47db3f1dd1df2dccbc48cd2c3c526e34d2809a
metal-face <hughesbryan3000@gmail.com>
2023-11-13 15:46:44 -0500

    Login page route added

--- src/router/index.rs
+++ src/router/index.rs
@@ -2,15 +2,19 @@ use yew::prelude::*;
 use yew_router::prelude::*;
 
 use crate::views::home::Home;
+use crate::views::login::Login;
 
 #[derive(Debug, Clone, Routable, PartialEq)]
 pub enum Route {
     #[at("/")]
     Home,
+    #[at("/login")]
+    Login,
 }
 
 pub fn switch(routes: Route) -> Html {
     match routes {
         Route::Home => html! { <Home /> },
+        Route::Login => html! { <Login />},
     }
 }
