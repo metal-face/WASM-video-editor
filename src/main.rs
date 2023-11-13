9abe68ab54837aae31f96df3222a07ce319f575c
metal-face <hughesbryan3000@gmail.com>
2023-11-12 23:03:38 -0500

    yew router added

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,15 @@
-use yew::prelude::*;
-use yew::{classes, html};
+mod router;
 
-#[function_component]
-fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
+use crate::router::index::{switch, Route};
+use yew::{function_component, html, Html};
+use yew_router::prelude::*;
 
+#[function_component(App)]
+pub fn app() -> Html {
     html! {
-        <div class={classes!("bg-red-400", "h-full")}>
-            <nav class={classes!("h-full")}>
-                <div class={classes!("bg-red-400", "h-full")}>
-                    <button {onclick}>{ "+1" }</button>
-                    <p class={classes!("text-emerald-600")}>{ *counter }</p>
-                </div>
-            </nav>
-        </div>
+        <BrowserRouter>
+            <Switch<Route> render={Switch::render(switch)} />
+        </BrowserRouter>
     }
 }
 
