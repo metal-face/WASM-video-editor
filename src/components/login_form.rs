125ae1cf9974b6644413aa4e2ad58cc7991a8543
metal-face <hughesbryan3000@gmail.com>
2023-11-14 12:09:33 -0500

    Login button added

--- src/components/login_form.rs
+++ src/components/login_form.rs
@@ -4,18 +4,16 @@ use yew::{function_component, html, Html};
 pub fn login_form() -> Html {
     html! {
         <div class="h-full w-full flex justify-center items-center">
-            <div class="w-1/2 h-1/2 flex flex-col items-center outline">
+            <div class="w-1/2 h-1/2 flex flex-col items-center outline rounded">
                 <div class="h-full w-full flex flex-col justify-center items-center ">
-                    <div class="flex flex-col">
-                        <label class="text-center" for="username">{"Username"}</label>
+                    <div class="flex flex-col m-1">
                         <input class="rounded p-1 shadow-" id="username" type="text" placeholder="Username" />
                     </div>
-                    <div class="flex flex-col">
-                        <label class="text-center" for="password">{"Password"}</label>
+                    <div class="flex flex-col m-1">
                         <input class="rounded p-1" id="password" type="password" placeholder="Password" />
                     </div>
-                    <div class="flex h-fit justify-items-end items-end">
-                        <button class="rounded p-1 shadow-xl bg-slate-600 text-white" type="button">{"Login"}</button>
+                    <div class="flex h-max align-end items-end">
+                        <button class="rounded p-1 shadow-xl bg-slate-600 text-white relative right-0 bottom-0" type="button">{"Login"}</button>
                     </div>
                 </div>
             </div>
