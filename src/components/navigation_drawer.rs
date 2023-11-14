5b05b3f9ddc354716e03e57e7dc937ab72727f4e
metal-face <hughesbryan3000@gmail.com>
2023-11-14 12:11:13 -0500

    whitespace removed

--- src/components/navigation_drawer.rs
+++ src/components/navigation_drawer.rs
@@ -5,19 +5,13 @@ pub struct Props {
     pub open: Callback<bool>,
 }
 
-
-
 #[function_component(NavigationDrawer)]
 pub fn navigation_drawer(props: &Props) -> Html {
-    
-
     if (props.open) {
         html! {
             <nav class="h-full w-1/3 bg-slate-600">
                 <button onclick={open.emit()}>{"X"}</button>
-
             </nav>
-
         }
     }
 }
