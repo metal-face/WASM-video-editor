d55252e4495f935c2abfd53db0459dbf40cc0ff3
metal-face <hughesbryan3000@gmail.com>
2023-11-14 12:09:26 -0500

    hamburger menu icon imported and used

--- src/components/app_bar.rs
+++ src/components/app_bar.rs
@@ -1,3 +1,4 @@
+use crate::icons::hamburger_menu::HamburgerMenu;
 use yew::{function_component, html, Html};
 use yew_router::components::Link;
 
@@ -6,9 +7,11 @@ use crate::router::index::Route;
 #[function_component(AppBar)]
 pub fn app_bar() -> Html {
     html! {
-        <div style="align-items: center;"  class="flex shadow-xl sticky h-12 w-full bg-slate-800">
-            <div class="h-auto w-1/2 pl-3">
-                <h1 class=" text-white">{"WASM Video Editor"}</h1>
+        <div class="flex items-center shadow-xl sticky h-12 w-full bg-slate-800">
+            <div class="h-max w-1/2 pl-3 text-white">
+                <button type="button" class="p-1 rounded-full hover:bg-slate-500 ease-in-out">
+                    <HamburgerMenu />
+                </button>
             </div>
             <div class="flex justify-end h-auto w-1/2">
                 <div class="h-fit text-white hover:underline m-1">
