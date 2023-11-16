09330473cc86b079d46214ee493678f4c12ad1cf
metal-face <hughesbryan3000@gmail.com>
2023-11-16 00:02:13 -0500

    refactored for parent child communication

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,54 @@ mod router;
 mod views;
 
 use crate::components::app_bar::AppBar;
+use crate::components::navigation_drawer::NavigationDrawer;
 use crate::router::index::{switch, Route};
-use yew::{function_component, html, Html};
+use yew::{html, Component, Context, Html};
 use yew_router::prelude::*;
 
-#[function_component(Main)]
-pub fn app() -> Html {
-    html! {
-        <BrowserRouter>
-            <main class="h-screen w-screen">
-                <AppBar />
-                <Switch<Route> render={switch} />
-            </main>
-        </BrowserRouter>
+pub enum Msg {
+    ButtonClick(bool),
+}
+pub struct App {
+    drawer_state: Option<bool>,
+}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(ctx: &Context<Self>) -> Self {
+        Self {
+            drawer_state: Some(true),
+        }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::ButtonClick(drawer_state) => {
+                self.drawer_state = Some(drawer_state);
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let on_closed = ctx.link().callback(Msg::ButtonClick);
+        let on_opened = ctx.link().callback(Msg::ButtonClick);
+
+        html! {
+            <BrowserRouter>
+                <main class="h-screen w-screen">
+                    <AppBar />
+
+                    <NavigationDrawer drawer_state={self.drawer_state} toggle_state={on_closed}/>
+                    <Switch<Route> render={switch} />
+                </main>
+            </BrowserRouter>
+        }
     }
 }
 
 fn main() {
-    yew::Renderer::<Main>::new().render();
+    yew::Renderer::<App>::new().render();
 }
