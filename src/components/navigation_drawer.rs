f90676919e7081ff7c2dc38d431ef2a95bce81ee
metal-face <hughesbryan3000@gmail.com>
2023-11-16 00:02:38 -0500

    refactored for parent child communication

--- src/components/navigation_drawer.rs
+++ src/components/navigation_drawer.rs
@@ -1,17 +1,38 @@
-use yew::{function_component, html, Html, Properties};
+use yew::{html, AttrValue, Callback, Component, Context, Html, Properties, UseStateHandle};
 
-#[derive(Properties, PartialEq)]
-pub struct Props {
-    pub open: Callback<bool>,
-}
+use super::*;
 
-#[function_component(NavigationDrawer)]
-pub fn navigation_drawer(props: &Props) -> Html {
-    if (props.open) {
-        html! {
-            <nav class="h-full w-1/3 bg-slate-600">
-                <button onclick={open.emit()}>{"X"}</button>
-            </nav>
+pub struct NavigationDrawer;
+
+#[derive(Clone, PartialEq, Properties)]
+pub struct NavDrawerProps {
+    pub drawer_state: Option<bool>,
+    pub toggle_state: Callback<bool>,
+}   
+
+impl Component for NavigationDrawer {
+    type Message = ();
+    type Properties = NavDrawerProps;
+
+    fn create(ctx: &yew::prelude::Context<Self>) -> Self {
+        Self {}
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let onclick = ctx.props().toggle_state.reform(move |_| false);
+        if ctx.props().drawer_state.unwrap() {
+            html! {
+                <nav class="h-full w-1/4 bg-slate-400">
+                    <div>
+                        <p>{"Good Day"}</p>
+                    </div>
+                    <button type="button" {onclick}>{"hell yeah"}</button>
+                </nav>
+            }
+        } else {
+            html! {
+                <></>
+            }
         }
     }
 }
