650dfe5a893553ab18ae378cace1fa9eee8fc249
metal-face <hughesbryan3000@gmail.com>
2023-11-16 16:09:08 -0500

    Ability to close drawer from app bar added

--- src/components/app_bar.rs
+++ src/components/app_bar.rs
@@ -9,6 +9,7 @@ pub struct AppBar;
 #[derive(Clone, PartialEq, Properties)]
 pub struct AppBarProps {
     pub close_drawer: Callback<bool>,
+    pub drawer_state: Option<bool>,
 }
 
 impl Component for AppBar {
@@ -20,24 +21,46 @@ impl Component for AppBar {
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        let onclick = ctx.props().close_drawer.reform(move |_| true);
+        let close_drawer = ctx.props().close_drawer.reform(move |_| false);
 
-        html! {
-            <div class="flex items-center shadow-xl sticky h-12 w-full bg-slate-800">
-                <div class="h-max w-1/2 pl-3 text-white">
-                    <button {onclick} type="button" class="p-1 rounded-full hover:bg-slate-500 ease-in-out">
-                        <HamburgerMenu />
-                    </button>
-                </div>
-                <div class="flex justify-end h-auto w-1/2">
-                    <div class="h-fit text-white hover:underline m-1">
-                        <Link<Route> to={Route::Home}>{"Home"}</Link<Route>>
+        let open_drawer = ctx.props().close_drawer.reform(move |_| true);
+
+        if ctx.props().drawer_state.unwrap().clone() {
+            html! {
+                <div class="flex items-center shadow-xl sticky h-12 w-full bg-slate-800">
+                    <div class="h-max w-1/2 pl-3 text-white">
+                        <button onclick={close_drawer} type="button" class="p-1 rounded-full hover:bg-slate-500 ease-in-out">
+                            <HamburgerMenu />
+                        </button>
                     </div>
-                    <div class="h-fit hover:underline m-1 text-white">
-                        <Link<Route> to={Route::Login}>{"Login"}</Link<Route>>
+                    <div class="flex justify-end h-auto w-1/2">
+                        <div class="h-fit text-white hover:underline m-1">
+                            <Link<Route> to={Route::Home}>{"Home"}</Link<Route>>
+                        </div>
+                        <div class="h-fit hover:underline m-1 text-white">
+                            <Link<Route> to={Route::Login}>{"Login"}</Link<Route>>
+                        </div>
                     </div>
                 </div>
-            </div>
+            }
+        } else {
+            html! {
+                <div class="flex items-center shadow-xl sticky h-12 w-full bg-slate-800">
+                    <div class="h-max w-1/2 pl-3 text-white">
+                        <button onclick={open_drawer} type="button" class="p-1 rounded-full hover:bg-slate-500 ease-in-out">
+                            <HamburgerMenu />
+                        </button>
+                    </div>
+                    <div class="flex justify-end h-auto w-1/2">
+                        <div class="h-fit text-white hover:underline m-1">
+                            <Link<Route> to={Route::Home}>{"Home"}</Link<Route>>
+                        </div>
+                        <div class="h-fit hover:underline m-1 text-white">
+                            <Link<Route> to={Route::Login}>{"Login"}</Link<Route>>
+                        </div>
+                    </div>
+                </div>
+            }
         }
     }
 }
