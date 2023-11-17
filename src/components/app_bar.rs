7ece99bcd12e793d2faf2762cf1b378c6b6ceede
metal-face <hughesbryan3000@gmail.com>
2023-11-17 03:14:39 -0500

    refactored to use the classes macro

--- src/components/app_bar.rs
+++ src/components/app_bar.rs
@@ -1,6 +1,6 @@
 use crate::components::github_login_button::GitHubLoginButton;
 use crate::icons::hamburger_menu::HamburgerMenu;
-use yew::{html, Callback, Component, Context, Html, Properties};
+use yew::{classes, html, Callback, Component, Context, Html, Properties};
 use yew_router::components::Link;
 
 use crate::router::index::Route;
@@ -27,17 +27,16 @@ impl Component for AppBar {
 
         if ctx.props().drawer_state.unwrap().clone() {
             html! {
-                <div class="flex items-center shadow-xl sticky h-24 z-20 w-full bg-slate-800">
-                    <div class="h-max w-1/2 pl-3 text-white">
-                        <button onclick={close_drawer} type="button" class="p-1 rounded-full hover:bg-slate-500 ease-in-out">
+                <div class={classes!("flex", "items-center", "shadow-xl", "sticky", "h-24",  "z-20", "w-full", "bg-slate-800")}>
+                    <div class={classes!("h-max", "w-1/2", "pl-3", "text-white")}>
+                        <button onclick={close_drawer} type="button" class={classes!("p-1", "rounded-full", "hover:bg-slate-500", "ease-in-out")}>
                             <HamburgerMenu />
                         </button>
                     </div>
-                    <div class="flex justify-end h-auto w-1/2">
-                        <div class="h-fit text-white hover:underline m-1">
+                    <div class={classes!("flex", "justify-end", "items-center", "h-auto", "w-1/2")}>
+                        <div class={classes!("h-fit", "text-white", "hover:underline", "text-xl", "m-1")}>
                             <Link<Route> to={Route::Home}>{"Home"}</Link<Route>>
                         </div>
-
                         <div class="h-fit m-1">
                             <GitHubLoginButton />
                         </div>
@@ -46,14 +45,14 @@ impl Component for AppBar {
             }
         } else {
             html! {
-                <div class="flex items-center shadow-xl sticky h-24 w-full bg-slate-800">
-                    <div class="h-max w-1/2 pl-3 text-white">
-                        <button onclick={open_drawer} type="button" class="p-1 rounded-full z-10  hover:bg-slate-500 ease-in-out">
+                <div class={classes!("flex", "items-center", "shadow-xl", "sticky", "h-24",  "z-20", "w-full", "bg-slate-800",)}>
+                    <div class={classes!("h-max", "w-1/2", "pl-3", "text-white")}>
+                        <button onclick={open_drawer} type="button" class={classes!("p-1", "rounded-full", "hover:bg-slate-500", "ease-in-out")}>
                             <HamburgerMenu />
                         </button>
                     </div>
-                    <div class="flex justify-end h-auto w-1/2">
-                        <div class="h-fit text-white hover:underline m-1">
+                    <div class={classes!("flex", "justify-end", "items-center", "h-auto", "w-1/2")}>
+                        <div class={classes!("h-fit", "text-white", "text-xl", "hover:underline", "m-1")}>
                             <Link<Route> to={Route::Home}>{"Home"}</Link<Route>>
                         </div>
 
