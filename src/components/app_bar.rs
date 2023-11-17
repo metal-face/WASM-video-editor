2e8bddb36cc0b1184216b37a62f64f3c69bd7178
metal-face <hughesbryan3000@gmail.com>
2023-11-17 02:42:21 -0500

    github login button added

--- src/components/app_bar.rs
+++ src/components/app_bar.rs
@@ -1,3 +1,4 @@
+use crate::components::github_login_button::GitHubLoginButton;
 use crate::icons::hamburger_menu::HamburgerMenu;
 use yew::{html, Callback, Component, Context, Html, Properties};
 use yew_router::components::Link;
@@ -16,18 +17,17 @@ impl Component for AppBar {
     type Message = ();
     type Properties = AppBarProps;
 
-    fn create(ctx: &Context<Self>) -> Self {
+    fn create(_ctx: &Context<Self>) -> Self {
         Self {}
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         let close_drawer = ctx.props().close_drawer.reform(move |_| false);
-
         let open_drawer = ctx.props().close_drawer.reform(move |_| true);
 
         if ctx.props().drawer_state.unwrap().clone() {
             html! {
-                <div class="flex items-center shadow-xl sticky h-12 w-full bg-slate-800">
+                <div class="flex items-center shadow-xl sticky h-24 z-20 w-full bg-slate-800">
                     <div class="h-max w-1/2 pl-3 text-white">
                         <button onclick={close_drawer} type="button" class="p-1 rounded-full hover:bg-slate-500 ease-in-out">
                             <HamburgerMenu />
@@ -37,17 +37,18 @@ impl Component for AppBar {
                         <div class="h-fit text-white hover:underline m-1">
                             <Link<Route> to={Route::Home}>{"Home"}</Link<Route>>
                         </div>
-                        <div class="h-fit hover:underline m-1 text-white">
-                            <Link<Route> to={Route::Login}>{"Login"}</Link<Route>>
+
+                        <div class="h-fit m-1">
+                            <GitHubLoginButton />
                         </div>
                     </div>
                 </div>
             }
         } else {
             html! {
-                <div class="flex items-center shadow-xl sticky h-12 w-full bg-slate-800">
+                <div class="flex items-center shadow-xl sticky h-24 w-full bg-slate-800">
                     <div class="h-max w-1/2 pl-3 text-white">
-                        <button onclick={open_drawer} type="button" class="p-1 rounded-full hover:bg-slate-500 ease-in-out">
+                        <button onclick={open_drawer} type="button" class="p-1 rounded-full z-10  hover:bg-slate-500 ease-in-out">
                             <HamburgerMenu />
                         </button>
                     </div>
@@ -55,8 +56,9 @@ impl Component for AppBar {
                         <div class="h-fit text-white hover:underline m-1">
                             <Link<Route> to={Route::Home}>{"Home"}</Link<Route>>
                         </div>
-                        <div class="h-fit hover:underline m-1 text-white">
-                            <Link<Route> to={Route::Login}>{"Login"}</Link<Route>>
+
+                        <div>
+                            <GitHubLoginButton />
                         </div>
                     </div>
                 </div>
